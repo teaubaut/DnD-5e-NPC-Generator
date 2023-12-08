69f5c9fd4c32259e4459f43d34846c5f14e200ca
Nathan Blair <36686768+teaubaut@users.noreply.github.com>
2023-12-07 18:34:16 -0600

    Feat: update race generator from function to class
    To incapsulate all race related code

--- non_player_character_generator.py
+++ non_player_character_generator.py
@@ -1,34 +1,60 @@
 import random
 import csv
 
-from dataclasses import dataclass
-
-
+# File paths for race and class data
 race_file_path = "./data/races.csv"
 class_file_path = "./data/classes.csv"
 
+# Maximum level for a non-player character
 max_level = 20
 
+# Alignment options on ethical and moral axes
 ethical_axis = ["Lawful", "Neutral", "Chaotic"]
 moral_axis = ["Good", "Neutral", "Evil"]
 
 
-@dataclass(kw_only=True)
-class NonPlayerCharacter:
-    npc_race: str
-    npc_class: str
-    npc_level: int
-    npc_alignment: str
+# Class representing the race of a non-player character
+class npc_race:
+    def __init__(self, npc_race) -> None:
+        # Initialize with a specific race or "random"
+        self.npc_race = npc_race
+        self.__post_init__()
+
+    def __post_init__(self):
+        # If race is set to "random", pick a random race
+        if self.npc_race == "random":
+            self.npc_race = self.get_random_race()
+
+    def get_random_race(self):
+        # Load race data and return a random race
+        races = load_from_csv(race_file_path)
+        return random.choice(races) if races else None
+
+
+# Class representing a non-player character, inheriting npc_race
+class NonPlayerCharacter(npc_race):
+    def __init__(self, npc_class, npc_level, npc_alignment, npc_race="random"):
+        # Initialize the base class (npc_race)
+        super().__init__(npc_race)
+        # Initialize additional NPC attributes
+        self.npc_class = npc_class
+        self.npc_level = npc_level
+        self.npc_alignment = npc_alignment
+
+    def __str__(self) -> str:
+        # String representation of the NPC
+        return f"Race: {self.npc_race} Class: {self.npc_class} Level: {self.npc_level} Alignment: {self.npc_alignment}"
 
 
 def main():
+    # Main function to create and display a non-player character
     non_player_character = NonPlayerCharacter(
-        npc_race=get_random_race(),
-        npc_class=get_random_class(),
-        npc_level=get_random_level(max_level),
-        npc_alignment=get_random_alignments(),
+        npc_race="random",  # Set the race as "random" or a specific race
+        npc_class=get_random_class(),  # Randomly select a class
+        npc_level=get_random_level(max_level),  # Randomly select a level
+        npc_alignment=get_random_alignments(),  # Randomly select an alignment
     )
-    print(non_player_character)
+    print(non_player_character)  # Print the NPC's detail
 
 
 def load_from_csv(file):
@@ -41,9 +67,9 @@ def load_from_csv(file):
         return []
 
 
-def get_random_race():
-    races = load_from_csv(race_file_path)
-    return random.choice(races) if races else None
+# def get_random_race():
+#     races = load_from_csv(race_file_path)
+#     return random.choice(races) if races else None
 
 
 def get_random_class():
